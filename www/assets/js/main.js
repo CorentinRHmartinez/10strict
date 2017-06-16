8847219de294247853f54ae7b58e172e963c73f4
corentinRHmartinez <mokuhter@gmail.com>
2017-06-16 10:07:09 +0200

    fixing again

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -264,39 +264,6 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
          * Portfolio
          /* ---------------------------------------------- */
 
-        var worksgrid   = $('#works-grid'),
-            worksgrid_mode;
-
-        if (worksgrid.hasClass('works-grid-masonry')) {
-            worksgrid_mode = 'masonry';
-        } else {
-            worksgrid_mode = 'fitRows';
-        }
-
-        worksgrid.imagesLoaded(function() {
-            worksgrid.isotope({
-                layoutMode: worksgrid_mode,
-                itemSelector: '.work-item'
-            });
-        });
-
-        $('#filters a').click(function() {
-            $('#filters .current').removeClass('current');
-            $(this).addClass('current');
-            var selector = $(this).attr('data-filter');
-
-            worksgrid.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-
-            return false;
-        });
-
 
         /* ---------------------------------------------- /*
          * Testimonials
